ca71b3351e7359797b3b328ee604cd792bfed7c7
VolobuevEA <VolobuevEA@samnipi.rosneft.ru>
2021-10-01 23:54:22 +0400

    выполнил задачу ss-51

---
+++ src/LI.BookService.Web/Controllers/DemandBooksController.cs
@@ -0,0 +1,41 @@
+﻿using LI.BookService.Core.Interfaces;
+using LI.BookService.Model.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+
+namespace LI.BookService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemandBooksController : ControllerBase
+    {
+        private IDemandBookService _demandBookService;
+
+        public DemandBooksController(IDemandBookService demandBookService)
+        {
+            _demandBookService = demandBookService;
+        }
+
+        /// <summary>
+        /// создание заявки на получение книги
+        /// </summary>
+        /// <param name="dtoDemandBook"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult> CreateDemandBook(DtoDemandBook dtoDemandBook)
+        {
+            if (dtoDemandBook != null)
+            {
+                await _demandBookService.CreateDemand(dtoDemandBook);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+    }
+}
