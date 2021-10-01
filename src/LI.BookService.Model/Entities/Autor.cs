3ed14b01dc3d03ee9d450151e2080484d232705f
Nefedov Vitaly <nefyodovvo@yandex.ru>
2021-10-02 01:18:01 +0400

    First stage (Nefedov)

---
+++ src/LI.BookService.Model/Entities/Autor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LI.BookService.Model.Entities
+{
+    public class Autor : IEntityBase
+    {
+        [Required]
+        [Key]
+        public int IdAutor { get; set; }
+        [Required]
+        [MaxLength(20, ErrorMessage = "Максимальная  длина составляет 20 символов")]
+        public string LastName { get; set; }
+        [MaxLength(20, ErrorMessage = "Максимальная  длина составляет 20 символов")]
+        public string FirstName { get; set; }
+    }
+}
---
+++ src/LI.BookService.Model/Entities/OfferList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LI.BookService.Model.Entities
+{
+    public class OfferList /*: IEntityBase*/
+    {
+        [Required]
+        [Key]
+        public int IdOfferList { get; set; }
+        [Required]
+        public int IdBookLiterary { get; set; }
+        [Required]
+        public int IdUser { get; set; }
+        [MaxLength(13, ErrorMessage = "Максимальная  длина составляет 13 символов")]
+        public string ISBN { get; set; }
+        [Required]
+        [MaxLength(4, ErrorMessage = "Максимальная  длина составляет 4 символа")]
+        public DateTime YearPublishing { get; set; }
+        [Required]
+        public DateTime CreateAt { get; set; }
+        [Required]
+        public DateTime UpdateAt { get; set; }
+        [Required]
+        public int IdStatus { get; set; }
+        [ForeignKey("IdBookLiterary")]
+        public BookLiterary BookLiterary { get; set; }
+        //[ForeignKey("IdUser")]
+        //public User User { get; set; }
+
+    }
+}
