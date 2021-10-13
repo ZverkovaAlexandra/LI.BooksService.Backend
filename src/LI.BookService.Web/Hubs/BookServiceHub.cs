057064f597f82b6664dd86b7b73ea3de4786f953
Vitaly Nefedov <nefyodovvo@yandex.ru>
2021-10-13 13:21:40 +0400

    Убрал код с примерами

--- src/LI.BookService.Web/Hubs/BookServiceHub.cs
+++ src/LI.BookService.Web/Hubs/BookServiceHub.cs
@@ -5,10 +5,5 @@ namespace LI.BookService.Hubs
 {
     public class BookServiceHub : Hub
     {
-        // ----- Example -----
-        public async Task Send(string message, string userName)
-        {
-            await Clients.All.SendAsync("Send", message, userName);
-        }
     }
 }
