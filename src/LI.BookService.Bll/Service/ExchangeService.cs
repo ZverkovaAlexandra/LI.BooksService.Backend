78c16186626e672ccbfcf4db4fe2db6597eb4d79
VolobuevEA <VolobuevEA@samnipi.rosneft.ru>
2021-10-20 16:21:55 +0400

    работаю над блоком задач SS-34

---
+++ src/LI.BookService.Bll/Service/ExchangeService.cs
@@ -0,0 +1,95 @@
+﻿using LI.BookService.Core.Interfaces;
+using LI.BookService.Model.DTO;
+using LI.BookService.Model.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LI.BookService.Bll.Service
+{
+    public class ExchangeService : IExchangeService
+    {
+        private IExchangeListRepository _exchangeListRepository;
+        private IOfferListRepository _offerListRepository;
+        private IWishListRepository _wishListRepository;
+        private IUserListRepository _userlistRepository;
+        private IUserValueCategoryRepository _userValueCategory;
+        public ExchangeService(IExchangeListRepository exchangeListRepository, IOfferListRepository offerListRepository, IWishListRepository wishListRepository, IUserListRepository userlistRepository, IUserValueCategoryRepository userValueCategory)
+        {
+            _exchangeListRepository = exchangeListRepository;
+            _offerListRepository = offerListRepository;
+            _wishListRepository = wishListRepository;
+            _userlistRepository = userlistRepository;
+            _userValueCategory = userValueCategory;
+        }
+
+        public async Task<List<DtoExchangeVariantsBook>> GetVariantesAsync(int exchangeListId)
+        {
+            var exchangeList = await _exchangeListRepository.GetByIdAsync(exchangeListId);
+
+            var offerlist = await _offerListRepository.GetByIdAsync(exchangeList.OfferList1Id); 
+            var wishList = await _wishListRepository.GetByIdAsync(exchangeList.WishList1Id);
+
+            var userListOfferType = await _userlistRepository.GetUserListAsync(offerlist.OfferListId, UserListType.OfferList);
+            var userListWishType = await _userlistRepository.GetUserListAsync(wishList.WishListId, UserListType.WishList);
+
+            var userValueCategoryOffer = await _userValueCategory.GetCategoriesByUserListIdAsync(userListOfferType.UserListId);// нашел категории из офферлистов
+            var userValueCategoryWish = await _userValueCategory.GetCategoriesByUserListIdAsync(userListWishType.UserListId);// нашел категории из вишлистов
+
+            var allCategory = userValueCategoryOffer.Union(userValueCategoryWish).ToList();///общий список категорий
+
+            var userValueCategories = await _userValueCategory.GetUserValueCategoryAsync(allCategory);//по списку категорий находим список сущностей UserValueCategory
+
+            var userListCategory = await _userlistRepository.GetExchangeUserListAsync();//находим все UserList в бд
+
+
+            //////////////// нужно уточнить у Александры
+            List<UserList> variantesListFull = new List<UserList>(); ///лист с полным совпадением по категориям
+            List<UserList> variantesListPart = new List<UserList>(); ///лист с частичным совпадением по категориям
+
+            foreach (var s in userListCategory)
+            {
+                if (userValueCategories.All(x => s.UserValueCategories.Contains(x)))
+                {
+                    variantesListFull.Add(s);
+                }
+                else
+                {
+                    while (userValueCategories.Count != 0)
+                    {
+                        userValueCategories.RemoveAt(userValueCategories.Count - 1);
+
+                        if (userValueCategories.All(x => s.UserValueCategories.Contains(x)))
+                        {
+                            variantesListPart.Add(s);
+                        }
+                    }
+                }
+            }
+            //////////////////////////////////
+            
+
+            var selectOfferLists = await _userlistRepository.GetOfferListAsync(variantesListFull);//находим все offer-листы,содержащие книги с  выбранными категориями
+
+            var selectBookLiteraries = await _offerListRepository.GetBookLiterariesAsync(selectOfferLists); //находим названия всех книг,которые содержатся в найденных офферлистах
+
+            var selectExchangeLists = await _exchangeListRepository.GetExchangeListsAsync(selectOfferLists);///находим все exchange-листы по найденным offer-листам
+            List<DtoExchangeVariantsBook> listVariantes = new List<DtoExchangeVariantsBook>();
+
+            for (int i=0;i<selectBookLiteraries.Count();i++)
+            {
+                DtoExchangeVariantsBook dto = new DtoExchangeVariantsBook();
+                dto.NameBook = selectBookLiteraries.ElementAt(i).BookName;
+                dto.ExchangeListId = selectExchangeLists.ElementAt(i).ExchangeListId;
+                listVariantes.Add(dto);
+            }
+
+
+            
+
+
+            return listVariantes;
+        }
+
+    }
+}
--- src/LI.BookService.DAL/Repositories/ExchangeListRepository.cs
+++ src/LI.BookService.DAL/Repositories/ExchangeListRepository.cs
@@ -1,5 +1,9 @@
 ﻿using LI.BookService.Core.Interfaces;
 using LI.BookService.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace LI.BookService.DAL.Repositories
 {
@@ -10,6 +14,26 @@ namespace LI.BookService.DAL.Repositories
         public ExchangeListRepository(BookServiceDbContext context) : base(context)
         {
             _context = context;
+        }
+
+        public async Task<List<ExchangeList>> GetListByIdAsync(int exchangeListId)
+        {
+            var list = await _context.ExchangeLists.Where(x => x.ExchangeListId == exchangeListId && x.IsBoth == true).ToListAsync();
+            return list;
+        }
+
+        public async Task<List<ExchangeList>> GetExchangeListsAsync(List<OfferList> offerLists)
+        {
+            List<ExchangeList> exchangeLists = new List<ExchangeList>();
+
+            foreach (var s in offerLists)
+            {
+                var exchangeList = await _context.ExchangeLists.FirstOrDefaultAsync(x => x.OfferList1Id == s.OfferListId);
+                if (exchangeList != null)
+                    exchangeLists.Add(exchangeList);
+            }
+
+            return exchangeLists;
         }
     }
 }
